650c582f542851ba265f78ec0b1ff3d7d7609dba
Monish <monish200110@gmail.com>
2022-06-13 11:40:47 +0530

    grid

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:agro/main.dart';
+// import 'package:agro/main.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-        child: Center(
-      child: Text(
-        "Home",
-        style: TextStyle(color: Colors.black),
+    return const Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: GridPaper(
+          color: Colors.blue,
+          divisions: 2,
+          interval: 200,
+          subdivisions: 10,
+        ),
       ),
-    ));
+    );
   }
 }
